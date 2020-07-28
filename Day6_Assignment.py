d67239c52b055e5393e3641477280badfd9dc2e8
yamini1987 <67826757+yamini1987@users.noreply.github.com>
2020-07-28 15:00:43 +0530

    Add files via upload

---
+++ Day6_Assignment.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+list1=["a","b","c","d","e"]#assignment Day_6
+list2=[1,2,3,4,5,6,7,8,9,10,11]
+res = {list1[i]: list2[i] for i in range(len(list1))}
+print ("dict : " +  str(res))
+
+
+# In[ ]:
+
+
+
+
